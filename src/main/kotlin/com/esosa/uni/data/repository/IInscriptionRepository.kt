bf176b67cc72aed2dd851a78c2fc6be8b352026b
EzeSosa <ezequielsosa423@gmail.com>
2024-05-11 18:08:45 -0300

    [RMV] [#4] Remove duplicated repositories

--- src/main/kotlin/com/esosa/uni/data/repository/IInscriptionRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.esosa.uni.data.repository
-
-import com.esosa.uni.data.model.Inscription
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface IInscriptionRepository : JpaRepository<Inscription, UUID> {}
